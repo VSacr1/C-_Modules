3c41a9b4cc56ea269d729d18fcad5695207f4138
Victoria <32303351+VSacr1@users.noreply.github.com>
2019-06-11 12:35:57 +0100

    Add files via upload

---
+++ Mod2_Lab4/Mod2_Lab4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a while loop
+            //we start with n = 1 
+            //the condition check for the while, tests if n is less than 6 
+            //if so the loop body code executes
+            //inside the loop we output the value of n and then increment it by 1
+            //once n = 6 the loop stops
+            //pay attention to the output to see what the last value is to ensure you understand
+            //how the evaluation of the condition is done and how the while loop executes
+
+            int n = 1;
+            while(n < 6)
+            {
+                Console.WriteLine($"Current value of n is {n}");
+                n++;
+            }
+        }
+    }
+}
---
+++ Mod2_Lab5/Mod2_Lab5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 8; i++)
+            {
+
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((i+j) % 2 == 0)
+                    {
+                        Console.Write("O");
+                    }
+                    else if((i + j) % 2 == 1)
+                    {
+                        Console.Write("X");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+            
+        }
+    }
+}
